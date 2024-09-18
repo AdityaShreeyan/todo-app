896ec075b2366813540b185bf35e0e2d4fdd43b5
AdityaShreeyan <shreeyanaditya@gmail.com>
2024-09-18 11:46:53 +0530

    Used Axios to make requests from React to Node.js backend and set up a basic to-do list UI with input for adding tasks and a list to display them.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TodoList from './components/TodoList.js';
+import AddTodo from './components/AddTodo';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function fetchTodos() {
+        const response = await axios.get('http://localhost:5000/api/todos');
+        setTodos(response.data);
+    }
+    fetchTodos();
+  }, []);
+
+  const addTodo = async (text) => {
+      const response = await axios.post('http://localhost:5000/api/todos', { text });
+      setTodos([...todos, response.data]);
+  };
+
+  const toggleCompleted = async (id) => {
+      const todo = todos.find(todo => todo._id === id);
+      const response = await axios.patch(`http://localhost:5000/api/todos/${id}`, { completed: !todo.completed });
+      setTodos(todos.map(t => t._id === id ? response.data : t));
+  };
+
+  const deleteTodo = async (id) => {
+      await axios.delete(`http://localhost:5000/api/todos/${id}`);
+      setTodos(todos.filter(todo => todo._id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <AddTodo addTodo={addTodo} />
+      <TodoList todos={todos} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo} />
     </div>
   );
 }
