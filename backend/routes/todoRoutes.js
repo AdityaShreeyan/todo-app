1e64b69570e85d4d2f20e0dd3308e58b7b2caa56
AdityaShreeyan <shreeyanaditya@gmail.com>
2024-09-19 14:55:39 +0530

    Undid some changes that caused issues.

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -4,9 +4,12 @@ const Todo = require('../models/Todo');
 
 // Get all todos
 router.get('/', async (req, res) => {
+  try {
     const todos = await Todo.find();
     res.json(todos);
- 
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Create a new todo
@@ -15,24 +18,38 @@ router.post('/', async (req, res) => {
     text: req.body.text
   });
 
+  try {
     const newTodo = await todo.save();
     res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 // Update a todo
 router.patch('/:id', async (req, res) => {
+  try {
     const todo = await Todo.findById(req.params.id);
+    if (!todo) return res.status(404).json({ message: 'Todo not found' });
 
     todo.completed = req.body.completed !== undefined ? req.body.completed : todo.completed;
     await todo.save();
     res.json(todo);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 // Delete a todo
 router.delete('/:id', async (req, res) => {
+  try {
     const todo = await Todo.findByIdAndDelete(req.params.id);
+    if (!todo) return res.status(404).json({ message: 'Todo not found' });
 
     res.json({ message: 'Todo deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
